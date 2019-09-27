07c134e28f432921955c9010508d8ba40bd02be7
Agustin Calatayud <acalatayud@itba.edu.ar>
2019-09-26 22:53:08 -0300

    Rename path error fix

--- src/image_file_renaming.py
+++ src/image_file_renaming.py
@@ -6,7 +6,7 @@ parser.add_argument("dir", help="directory where images are stored", type=str)
 args = parser.parse_args()
 for filename in os.listdir(args.dir):
     if filename.endswith(".png"):
-        src = filename
-        dst = "img" + filename
+        src = os.path.join(args.dir, filename)
+        dst = os.path.join(args.dir, "img" + filename)
         os.rename(src, dst)
 
